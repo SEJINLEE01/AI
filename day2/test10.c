b7174814248251e7d39b997297de887d75606c0e
LEESEJIN <stacrft@gmail.com>
2025-12-24 14:33:43 +0900

    string

---
+++ day2/test10.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+    float ft = 1.234567890123456789;
+    double db = 1.234567890123456789;
+
+    printf("%.20f\n",ft);
+    printf("%.20lf\n",db);
+}
---
+++ day2/test9.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <limits.h>
+
+int main(){
+    unsigned int a = UINT_MAX;
+    
+    printf("%d\n",a);
+    printf("%u\n",a); 
+
+    return 0;
+}
