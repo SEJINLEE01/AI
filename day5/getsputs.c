af4abf09a0492ffa370af306e28e37e703a7f140
LEESEJIN <stacrft@gmail.com>
2025-12-31 11:44:46 +0900

    half5day

--- day5/getsputs.c
+++ day5/getsputs.c
@@ -2,11 +2,12 @@
 
 int main(){
     char str[80];
-
+    int count = sizeof(str);
     printf("문자열 입력 : ");
     // gets(str); // 띄워쓰기 저장
-    scanf("%s",str); // 띄워쓰기 저장 x
+    // scanf("%s",str); // 띄워쓰기 저장 x
     // gets는 배열의 칸수 같은거 확인안하고 다집어넣는다(오버플로어 발생), scanf는 딱 그 문자열 크기만큼만 넣어준다. 그래서 gets는 잘 안쓴다.
+    fgets(str,count,stdin);
     puts("입력된 문자열 : ");
     puts(str);
 }
---
+++ day5/notnullstr.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    char str[5];
+
+    str[0] = 'O';
+    str[1] = 'K';
+    printf("%d\n",str[4]);
+    printf("%s\n",str);
+}
