29e099730f3cb54ab393da14d72343bbd53d7acd
LEESEJIN <stacrft@gmail.com>
2025-12-31 16:40:58 +0900

    end5day

--- day5/ex4.c
+++ day5/ex4.c
@@ -5,7 +5,7 @@ int main(){
     int N;
     double* battery;
     int warning = 0;
-
+    int ON = 30, OFF = 35;
     printf("몇개의 상황? : ");
     scanf("%d",&N);
     battery = (double*)malloc(sizeof(double)*N);
@@ -16,8 +16,8 @@ int main(){
     }
     printf("\n");
     for(int i =0;i<N;i++){
-        if(warning==0 && battery[i]<=30) warning = 1;
-        else if(warning==1 && battery[i]>=35) warning = 0;
+        if(warning==0 && battery[i]<=ON) warning = 1;
+        else if(warning==1 && battery[i]>=OFF) warning = 0;
 
         printf("%2d번째 배터리의 잔량 %.2lf%% / 경고 %s\n",i+1,battery[i],warning==1?"ON":"OFF");
     }
